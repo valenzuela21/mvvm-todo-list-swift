9b8f61ea0411aaf10a47b3157dabe47f26100fdc
David Valenzuela <vlzdavid12@outlook.com>
2025-07-27 17:14:03 -0500

    Initial Commit

---
+++ todolistTests/todolistTests.swift
@@ -0,0 +1,17 @@
+//
+//  todolistTests.swift
+//  todolistTests
+//
+//  Created by David Valenzuela Pardo on 27/07/25.
+//
+
+import Testing
+@testable import todolist
+
+struct todolistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
